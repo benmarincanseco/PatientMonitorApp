f5d05188fe937a00141bbf2ea879b8cb2ac7066d
Benjamin <60583572+benmarincanseco@users.noreply.github.com>
2022-02-15 09:25:58 -0500

    device module revision

---
+++ device.py
@@ -0,0 +1,11 @@
+class Device:
+
+    def __init__(device, userId, function, metric):
+        device.userId = name
+        device.function = age
+        device.metric = metric
+    def metricRetreive(self):
+        return self.metric
+
+    def metricUpdate(self,metric):
+        self.metric = metric
