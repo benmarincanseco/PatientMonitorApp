e7c2325b88afa3cc88132fa2316f4d290a83e999
Benjamin <60583572+benmarincanseco@users.noreply.github.com>
2022-02-22 03:47:55 -0500

    Fix linter errors

--- device.py
+++ device.py
@@ -2,13 +2,14 @@ import json
 import jsonschema
 import os
 
+
 def validateData(fileName):
-    file=fileName+".json"
+    file =f ileName + ".json"
     with open('deviceSchema.json') as deviceSchema:
         schema = json.load(deviceSchema)
     validator = jsonschema.Draft7Validator(schema)
     if os.path.isfile(file) and os.access(file, os.R_OK):
-        with open(file,'r') as f:
+        with open(file, 'r') as f:
             data = json.load(f)
         return list(validator.iter_errors(data))
     else:
