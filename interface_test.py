4a46cf0f84f68db579594f113c7662050360d79c
Benjamin <60583572+benmarincanseco@users.noreply.github.com>
2022-02-28 23:15:54 -0500

    Increase Testing

--- interface_test.py
+++ interface_test.py
@@ -1,7 +1,49 @@
 import device
 
 
-def testTest():
+def testNoValidFile():
     actual = device.validateData("")
     expected = ["Invalid file"]
+    assert actual == expected
+
+
+def testInvalidID1():
+    actual = device.validateData("invalidID1")
+    expected = [<ValidationError: '-102 is less than the minimum of 1'>]
+    assert actual == expected
+
+
+def testInvalidID2():
+    actual = device.validateData("invalidID2")
+    expected = [<ValidationError: "'car' is not of type 'integer'">]
+    assert actual == expected
+
+
+def testInvalidType():
+    actual = device.validateData("invalidType")
+    expected = [<ValidationError: "'RaceCar' is not one of ['Temperature', 'Blood Pressure', 'Pulse', 'Oximeter', 'Weight', 'Glucometer']">]
+    assert actual == expected
+
+
+def testInvalidType():
+    actual = device.validateData("invalidType")
+    expected = [<ValidationError: "'1' is not of type 'number'">]
+    assert actual == expected
+
+
+def testMissingTime():
+    actual = device.validateData("invalidType")
+    expected = [<ValidationError: "None is not of type 'integer'">, <ValidationError: "None is not of type 'integer'">]
+    assert actual == expected
+
+
+def testValid():
+    actual = device.validateData("valid")
+    expected = [<ValidationError: "None is not of type 'integer'">, <ValidationError: "None is not of type 'integer'">]
+    assert actual == expected
+
+
+def testMultipleErrors():
+    actual = device.validateData("multiError")
+    expected = [<ValidationError: '-100 is less than the minimum of 1'>, <ValidationError: "2 is not one of ['Temperature', 'Blood Pressure', 'Pulse', 'Oximeter', 'Weight', 'Glucometer']">, <ValidationError: "'Train' is not of type 'integer'">, <ValidationError: "None is not of type 'number'">]
     assert actual == expected
