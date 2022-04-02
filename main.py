e3f36fbdc6a3fd6083cd12d1927889ef9d94ec84
Benjamin <60583572+benmarincanseco@users.noreply.github.com>
2022-04-02 12:14:48 -0400

    comments

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flask import Flask
-from flask_restful import Api, Resource
-import json
-
-with open('valid.json') as deviceSchema:
-    schema = json.load(deviceSchema)
-##remove debug = true
-app = Flask(__name__)
-api = Api(app)
-class Helloworld(Resource):
-    def get(self):
-        return schema
-
-api.add_resource(Helloworld, "/")
-
-
-if __name__ == "__main__":
-	app.run(debug=True)
+# from flask import Flask
+# from flask_restful import Api, Resource
+# import json
+#
+# with open('valid.json') as deviceSchema:
+#     schema = json.load(deviceSchema)
+# ##remove debug = true
+# app = Flask(__name__)
+# api = Api(app)
+# class Helloworld(Resource):
+#     def get(self):
+#         return schema
+#
+# api.add_resource(Helloworld, "/")
+#
+#
+# if __name__ == "__main__":
+# 	app.run(debug=True)
